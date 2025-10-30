e665590061d759bcbea2d05ac97d97e5723ffe86
Himanshu <himanshugaur684@gmail.com>
2025-10-30 15:34:17 +0530

    State Hoisting.

--- app/src/main/java/dev/himanshu/composebasics/MainActivity.kt
+++ app/src/main/java/dev/himanshu/composebasics/MainActivity.kt
@@ -4,9 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import dev.himanshu.composebasics.ui.theme.ComposeBasicsTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,10 +30,47 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeBasicsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    BadCOmposable(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    )
                 }
             }
         }
     }
 }
- 
+
+
+@Composable
+fun BadCOmposable(modifier: Modifier = Modifier) {
+    var counter by rememberSaveable { mutableIntStateOf(0) }
+
+    Counter(
+        modifier = modifier.fillMaxSize(),
+        counter = counter,
+        onCounterChanged = { counter++ })
+
+}
+
+@Composable
+fun Counter(
+    modifier: Modifier = Modifier,
+    counter: Int,
+    onCounterChanged: () -> Unit
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Text(counter.toString())
+        Spacer(Modifier.height(10.dp))
+        Button(onClick = { onCounterChanged.invoke() }) {
+            Text("Increase Counter")
+        }
+    }
+}
+
+
