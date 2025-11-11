62e92e0e7b57fed71994a30ab4e24b57556a936b
Himanshu <himanshugaur684@gmail.com>
2025-11-11 11:44:31 +0530

    fixed

--- app/src/main/java/dev/himanshu/composebasics/MainActivity.kt
+++ app/src/main/java/dev/himanshu/composebasics/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import dev.himanshu.composebasics.ui.theme.ComposeBasicsTheme
 
@@ -22,4 +26,11 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
- 
+
+
+@Composable
+fun ExampleBox(modifier: Modifier = Modifier) {
+    Box(modifier=modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+        Text("This is my main branch.")
+    }
+}
